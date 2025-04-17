607abd6c40094d5fe6ca0a949eca0b96c18cb2c8
Yuuki883 <yuukisaitou14@gmail.com>
2025-04-18 08:17:40 +0900

    update:クライアント側の設定更新

--- next.config.js
+++ next.config.js
@@ -1,6 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // ...
+  reactStrictMode: true,
+  images: {
+    domains: [
+      'crests.football-data.org',
+      'upload.wikimedia.org',
+      'tmssl.akamaized.net',
+      'www.thesportsdb.com',
+    ],
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      // クライアントサイドでは以下のモジュールを使わない
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        net: false,
+        tls: false,
+        fs: false,
+        dns: false,
+      };
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
